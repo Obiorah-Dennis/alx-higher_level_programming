8a812ed03a314c2538fb65c9c46f3fdeb274ba75
Obiorah-Dennis <obiorahdennis@yahoo.com>
2023-05-16 07:42:23 -0700

    list all in table

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- full description
+mysql -u your_username -p your_database_name -e "SHOW CREATE TABLE first_table;"
+
---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+-- list all in table
+mysql -u hlocalhost -p hbtn_0c_0 -e "SELECT * FROM first_table;"
+
